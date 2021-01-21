a31a6881ba619ef36c91875c19360fb05a02a2ac
Fachri M Fulchan <66100942+FachriMFulchan@users.noreply.github.com>
2021-01-21 09:05:05 +0700

    Upload Folder

---
+++ 1_Basic/1_Variabel/1_Variabel.py
@@ -0,0 +1,25 @@
+# Variabel adalah tempat menyimpat data
+
+#menaro / assignment nilai
+#di python tuh gaada deklarasi
+a = 10
+x = 5
+panjang = 1000
+
+print ('Nilai a = ', a)
+print ('Nilai x = ', x)
+print ('Nilai panjang = ', panjang)
+
+#penamaan
+nilai_y = 15 #dengan menggunakan undersore
+juta10 = 10000000 #gaboleh depannya angka
+nilaiZ = 17.5 #biasanya camel style
+
+#pemanggilan kedua
+print ('Nilai a = ', a)
+a = 7
+print ('Nilai a = ', a)
+
+#assignment indirect
+b = a
+print ('Nilai b = ', b)
